3c7c01b80139b708436f84cc8c99b46c9dba5bf2
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2019-03-21 17:20:26 +0800

    初步接入 page-creator-ide

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,20 @@
+var proxy = require("http-proxy-middleware")
 module.exports = {
-    pathPrefix: "/ide-blog",
+  pathPrefix: "/ide-blog",
+  plugins: [`gatsby-plugin-react-helmet`],
+  developMiddleware: app => {
+    app.use(
+      [
+        "/page-creator/json.worker.js",
+        "/page-creator/editor.worker.js",
+        "/page-creator/typescript.worker.js",
+      ],
+      proxy({
+        target: "//unpkg.com/ide-code-editor/dist/",
+          pathRewrite:{
+              "/page-creator/": "/"
+          }
+      })
+    )
+  },
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import React from "react"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
+
 export default () => (
-    <div style={{ color: `purple` }}>
+  <div style={{ color: `purple` }}>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>首页</title>
+    </Helmet>
     <h1>Hello Buddy!</h1>
     <p>Welcome to IDE World</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
