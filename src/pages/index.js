a8116446ea18fad683f41d5d2e5d0ab5e83ff976
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2019-03-21 14:40:10 +0800

    feat(初始化): 使用 gatsby 初始化网站

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import {Link} from "gatsby"
+export default () => (
+    <div style={{ color: `purple` }}>
+    <h1>Hello Buddy!</h1>
+    <p>Welcome to IDE World</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
 
-export default () => <div>Hello world!</div>
+    <br />
+    <div>
+      <Link to="/page-creator/">page-creator-ide - 页面可视化搭建平台</Link>
+    </div>
+  </div>
+)
