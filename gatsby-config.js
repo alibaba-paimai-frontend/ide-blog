5797906e454493f72df5f1d828da2c306f49213d
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2019-03-25 17:40:58 +0800

    add markdown support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,15 @@
 var proxy = require("http-proxy-middleware")
 module.exports = {
   pathPrefix: "/ide-blog",
-  plugins: [`gatsby-plugin-react-helmet`]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/markdowns/`,
+        name: "markdown-pages",
+      },
+    },
+    `gatsby-transformer-remark`
+  ],
 }
---
+++ src/markdowns/how-create-ide-component.md
@@ -0,0 +1,7 @@
+---
+path: "/blog/how-create-ide-component"
+date: "2019-03-25"
+title: "How To Create A IDE Component"
+---
+
+gggggg
